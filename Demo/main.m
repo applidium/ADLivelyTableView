fbc5ec0d822f999bc572cfae372fd83b4e2f9a41
Romain Goyet <romain.goyet@applidium.com>
2012-04-24 19:22:47 +0200

    Adding a demo

---
+++ Demo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  LivelyDemo
+//
+//  Created by Romain Goyet on 24/04/12.
+//  Copyright (c) 2012 Applidium. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "LDAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([LDAppDelegate class]));
+    }
+}
